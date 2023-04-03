2d66986c05352f3ed89b5415120bc34c47d81235
Christian Hockenberger <christian@mediaberger.com>
2023-04-03 23:21:57 +0200

    Added missing file

---
+++ cho/Config.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Cho;
+
+class Config {
+
+	protected $env  = null;
+	protected $name = null;
+	protected $root = null;
+
+	protected $images_salt = null;
+	protected $images_base = null;
+
+	protected $website_login        = null;
+	protected $website_password     = null;
+	protected $website_receiver_url = null;
+	protected $website_get_tags_url = null;
+
+	/**/
+
+	function __construct() {
+		$cfg = CHO_CONFIG;
+		$this->env                   = $cfg->get( 'app/env' );
+		$this->name                  = $cfg->get( 'app/name' );
+		$this->root                  = $cfg->get( 'app/home_url' );
+		$this->images_salt           = $cfg->get( 'images/salt' );
+		$this->images_base           = $cfg->get( 'images/base' );
+		$this->website_login         = $cfg->get( 'website/login' );
+		$this->website_password      = $cfg->get( 'website/password' );
+		$this->website_rest_root_url = $cfg->get( 'website/rest_root_url' );
+		$this->website_receiver_url  = $cfg->get( 'website/receiver_url' );
+		$this->website_get_tags_url  = $cfg->get( 'website/get_tags_url' );
+	}
+
+	/**/
+
+	public function get_env() {
+		return $this->env;
+	}
+
+	public function get_name() {
+		return $this->name;
+	}
+
+	public function get_root() {
+		return $this->root;
+	}
+
+	public function get_images_salt() {
+		return $this->images_salt;
+	}
+
+	public function get_images_base() {
+		return $this->images_base;
+	}
+
+	public function get_website_login() {
+		return $this->website_login;
+	}
+
+	public function get_website_password() {
+		return $this->website_password;
+	}
+
+	public function get_website_rest_root_url() {
+		return $this->website_rest_root_url;
+	}
+
+	public function get_website_receiver_url() {
+		return $this->website_receiver_url;
+	}
+
+	public function get_website_get_tags_url() {
+		return $this->website_get_tags_url;
+	}
+
+}
